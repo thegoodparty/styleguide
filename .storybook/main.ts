10b6396e60de55f21f7a5aae075220403e5c4e92
Israel Vicars <israelvicars@gmail.com>
2025-05-29 17:50:44 -0400

    feat: storybook theming

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,9 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/experimental-addon-test',
   ],
