f855ca3c9cbf3cf7233be7609fb5a2cd8ba32be1
Israel Vicars <israel@goodparty.org>
2025-05-30 11:20:06 -0400

    [WEB-4221] Typography & Input styling (#4)
    * feat: typography support
    
    * feat: standardize input heights
    
    * feat: storybook theming
    
    * fix: remove extraneous divs
    
    * chore: update linter rules, scripts
    
    * chore: make linter rules respect generated files, remove semicolons elsewhere

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Style Guide',
+  brandUrl: '/',
+  brandImage: '/favicon/apple-touch-icon.png',
+  brandTarget: '_self',
+}) 
--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -14,7 +14,7 @@ const toggleVariants = cva(
           "border border-input bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-9 px-2 min-w-9",
+        default: "h-10 px-2 min-w-10",
         sm: "h-8 px-1.5 min-w-8",
         lg: "h-10 px-2.5 min-w-10",
       },
